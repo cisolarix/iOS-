a29d6b11c8f1191a8a0a016c05b6e8c0fb3c51e6
Yanming Deng <cisolarix@gmail.com>
2014-09-16 13:29:21 +0800

    Initial Commit

---
+++ 通讯录/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  通讯录
+//
+//  Created by Yanming Deng on 9/16/14.
+//  Copyright (c) 2014 Yanming Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 通讯录/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  通讯录
+//
+//  Created by Yanming Deng on 9/16/14.
+//  Copyright (c) 2014 Yanming Deng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
