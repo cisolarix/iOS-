bcf8ba69fa2e2b0f7c701938ad1e8e0b6dad1373
Yanming Deng <cisolarix@gmail.com>
2014-09-16 13:55:58 +0800

    简单访问

--- 通讯录/ViewController.m
+++ 通讯录/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AddressBook/AddressBook.h>
 
 @interface ViewController ()
 
@@ -16,12 +17,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self requestAddressBook];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)requestAddressBook {
+    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+    ABAddressBookRequestAccessWithCompletion(addressBook, ^(bool granted, CFErrorRef error) {
+        
+        if (granted) {
+            NSLog(@"允许访问");
+            [self readAddressBook];
+        } else {
+            NSLog(@"不允许访问");
+        }
+    });
+    
+    
+}
+
+- (void)readAddressBook {
+    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+    CFArrayRef allPeople = ABAddressBookCopyArrayOfAllPeople(addressBook);
+    CFIndex count = CFArrayGetCount(allPeople);
+    for (CFIndex i=0; i<count; i++) {
+        ABRecordRef person = CFArrayGetValueAtIndex(allPeople, i);
+        CFStringRef firstName = ABRecordCopyValue(person, kABPersonFirstNameProperty);
+        CFStringRef lastName = ABRecordCopyValue(person, kABPersonLastNameProperty);
+        
+        NSLog(@"%@, %@", firstName, lastName);
+        CFRelease(firstName);
+        CFRelease(lastName);
+        CFRelease(person);
+    }
+    CFRelease(addressBook);
+    CFRelease(allPeople);
 }
 
 @end
